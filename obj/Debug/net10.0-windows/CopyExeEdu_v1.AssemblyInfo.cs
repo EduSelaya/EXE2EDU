c18e1dd5af41403ae4ddbe446867653aff7b34e5
EduSelaya <eselaya@hotmail.com>
2026-02-28 13:06:12 -0300

    Se compilo el proyecto para trabajar con NET10.0

--- obj/Debug/net10.0-windows/CopyExeEdu_v1.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/CopyExeEdu_v1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CopyExeEdu_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016a7852ead0a991cb1efa6b657840ea9eb3f5f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d43884308ae73cd3bc0294718090ae3ea1d79e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CopyExeEdu_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CopyExeEdu_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
